3d63aa5d48e4e68e211efbd899498fd65662eb18
muzaffercanan <109433767+muzaffercanan@users.noreply.github.com>
2023-11-30 13:50:47 +0300

    Update IDBSTNode.java

--- IDBSTNode.java
+++ IDBSTNode.java
@@ -1,9 +1,11 @@
 public class IDBSTNode {
     int ID;
     IDBSTNode left, right;
+    Student student;
 
     public IDBSTNode(int ID) {
         this.ID = ID;
         this.left = this.right = null;
+        this.student=student;
     }
 }
