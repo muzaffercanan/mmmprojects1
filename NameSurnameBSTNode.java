422275389d9922eec85541175557037cc224b626
muzaffercanan <109433767+muzaffercanan@users.noreply.github.com>
2023-11-30 13:51:14 +0300

    Update NameSurnameBSTNode.java

--- NameSurnameBSTNode.java
+++ NameSurnameBSTNode.java
@@ -1,9 +1,11 @@
 public class NameSurnameBSTNode {
     String nameSurname;
     NameSurnameBSTNode left, right;
+    Student student;
 
     public NameSurnameBSTNode(String nameSurname) {
         this.nameSurname = nameSurname;
         this.left = this.right = null;
+        this.student=student;
     }
 }
